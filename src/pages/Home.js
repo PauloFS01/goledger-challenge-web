4325898b1ada068d0361b2f89780249247ecac7f
Paulo Silva <paulosilva@Paulos-MacBook-Air.local>
2022-08-03 12:08:32 -0300

    disable behavior button add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -74,6 +74,7 @@ export default function Home() {
                   variant="contained"
                   startIcon={<AddIcon />}
                   onClick={handleOpen}
+                  disabled={!selectedAsset}
                 >
                   Add asset
                 </Button>
